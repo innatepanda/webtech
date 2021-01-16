dcf529c40416216bb6237e28c75935ae20c9dc2c
virtualpanda <1905228@kiit.ac.in>
2021-01-16 14:25:02 +0530

    cant redirect

--- actualToDList/app.js
+++ actualToDList/app.js
@@ -1,11 +1,18 @@
 const input=document.querySelector('.todo_input')
 const button=document.querySelector('.submit_button')
 const list=document.querySelector('.todo_list')
-
+const mainbtn=document.querySelector('.main_submit')
 button.addEventListener("click", add);
+mainbtn.addEventListener("click", submit);
 
-
-
+function submit(event){
+    //event.preventDefault();
+    //const namev=document.querySelector('.name');
+    
+    window.location.href='http://stackoverflow.com';
+    //const he=document.querySelector('.header');
+    //he.innerText=name.value+" ,Welcome";
+}
 function add(event)
 {
     
@@ -20,23 +27,28 @@ function add(event)
     div.classList.add("todo")
     
     const li=document.createElement('li');
+    var currentTime = (new Date()).toDateString();
     li.innerText= input.value;
+    li.innerHTML=currentTime+'<br><br>'+li.innerText;
     input.value="";
     li.classList.add("todo_item");
+    //div.append(currentTime);
     div.appendChild(li);
 
     const checked=document.createElement('button');
     checked.innerHTML='<i class="fas fa-check"></i>'
     checked.classList.add("completed_btn");
-
-    div.appendChild(checked);
+    checked.addEventListener("click", deleteFunc);
+    
 
     const trash=document.createElement('button');
     trash.innerHTML='<i class="fas fa-trash"></i>'
     trash.classList.add("delete_btn");
     trash.addEventListener("click", deleteFunc);
 
+    div.appendChild(checked);
     div.appendChild(trash);
+    
     list.appendChild(div);
 
 
@@ -45,5 +57,17 @@ function deleteFunc(event)
 {
     event.preventDefault();
     const item=event.target;
+    if(item.classList[0]=="delete_btn")
+    {
+        const parent=item.parentElement;
+        parent.remove();
+    }
+    if(item.classList[0]=="completed_btn")
+    {
+        const parent=item.parentElement;
+        parent.classList.toggle("completed");
+        
+    }
+
 
 }
